869b59233ea5ffc4b22c294940ada1b26fe74486
vilarion <vilarion@illarion.org>
2011-06-16 11:11:07 +0200

    added copyright and license information to each source file

--- playersave.hh
+++ playersave.hh
@@ -1,3 +1,22 @@
+//  illarionserver - server for the game Illarion
+//  Copyright 2011 Illarion e.V.
+//
+//  This file is part of illarionserver.
+//
+//  illarionserver is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  illarionserver is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with illarionserver.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #ifndef __playersave_hh
 #define __playersave_hh
 
--- utility.hpp
+++ utility.hpp
@@ -1,2 +1,21 @@
+//  illarionserver - server for the game Illarion
+//  Copyright 2011 Illarion e.V.
+//
+//  This file is part of illarionserver.
+//
+//  illarionserver is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  illarionserver is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with illarionserver.  If not, see <http://www.gnu.org/licenses/>.
+
+
 extern bool mypred(char c1, char c2);
 extern bool comparestrings_nocase( const std::string& s1, const std::string& s2 );
