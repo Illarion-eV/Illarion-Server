170b4b88d4b56b09400d4b94ae337786d0e9c98e
Martin Karing <nitram@illarion.org>
2011-07-02 23:22:02 +0200

    Resolving pathes and replace the old with the new database binding

--- db/ConnectionManager.cpp
+++ db/ConnectionManager.cpp
@@ -16,290 +16,74 @@
 //  You should have received a copy of the GNU General Public License
 //  along with illarionserver.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "dbng/ConnectionManager.hpp"
+#include "dbng/Connection.hpp"
 
-#include "ConnectionManager.hpp"
-
-#include <stdexcept>
-#include <algorithm>
-#include <iomanip>
 #include <sstream>
 #include <string>
+#include <stdexcept>
 
-boost::shared_ptr<ConnectionManager> dbmgr;
-boost::shared_ptr<ConnectionManager> accdbmgr;
+#include <boost/cstdint.hpp>
+#include <pqxx/connection.hxx>
 
-//! \brief constructor replacement (factory function)
-//!
-//! \param user
-//!     the database user, may be ""
-//! \param password
-//!     the password of the user, may be ""
-//! \param database
-//!     the database for the connection, must not be ""!
-//! \param host
-//!     the hostname for the connection, may be "" for localhost
-//! \param port
-//!     the port for the connection, may be ""
-//!
-//! this function creates the initial connections
-//!
-boost::shared_ptr<ConnectionManager>
-ConnectionManager::CreateConnectionManager(
-    const std::string &user,
-    const std::string &password,
-    const std::string &database,
-    const std::string &host,
-    const std::string &port,
-    int initialConnectionCount) {
-    // std::cerr << "creating Connection Manager with parameters: " <<
-    // "user=" << user << ", password=" << password << ", database=" << database << ", host=" << host << ", port=" << port << std::endl;
-    // construct
-    boost::shared_ptr<ConnectionManager> conmgr(
-        new ConnectionManager(user, password, database, host, port, initialConnectionCount)
-    );
+using namespace Database;
 
-    // set weak pointer
-    conmgr->this_weak = conmgr;
+ConnectionManager ConnectionManager::instance;
 
-    // build initial connections
-    conmgr->createConnections(initialConnectionCount);
-
-    // get a db connection with transaction
-    TransactionHolder trns = conmgr->getTransaction();
-
-    return conmgr;
+PConnectionManager ConnectionManager::getInstance() {
+    return &ConnectionManager::instance;
 }
 
-
-//! \brief constructor (private, construct with CreateConnectionManager)
-//!
-//! \param user
-//!     the database user, may be ""
-//! \param password
-//!     the password of the user, may be ""
-//! \param database
-//!     the database for the connection, must not be ""!
-//! \param host
-//!     the hostname for the connection, may be "" for localhost
-//! \param port
-//!     the port for the connection, may be ""
-//!
-ConnectionManager::ConnectionManager(
-    const std::string &user,
-    const std::string &password,
-    const std::string &database,
-    const std::string &host,
-    const std::string &port,
-    int initialConnectionCount):
-    con_info(user, password, database, host, port) {}
-
-//! \brief destructor - kills all used connections, frees all unused
-//!
-//! rolls back all transactions currently open (with logging to stderr)
-//!
-ConnectionManager::~ConnectionManager() throw() {
-    boost::recursive_mutex::scoped_lock lock(mutex); // protect
-
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-
-    // roll back all transactions
-    std::map< int, boost::weak_ptr<Transaction> >::iterator itTrns;
-
-    for (itTrns = transactionMap.begin(); itTrns != transactionMap.end(); ++itTrns) {
-        try {
-            // may throw bad_weak_ptr
-            boost::shared_ptr<Transaction> pt(itTrns->second.lock());
-
-            std::cerr << __PRETTY_FUNCTION__ << "rolling back transaction" << std::endl;
-            pt->rollback();
-        } catch (const boost::bad_weak_ptr &) {
-            // that's ok
-            std::cerr << __PRETTY_FUNCTION__ << "found dangling transaction" << std::endl;
-        } catch (...) {
-            // that's not ok but we can't do anything
-            std::cerr << __PRETTY_FUNCTION__ << "unknown error in destructor" << std::endl;
-        }
+PConnection ConnectionManager::getConnection() {
+    if (!this->isReady) {
+        throw new std::domain_error("Connection Manager is yet not setup");
     }
 
-    // clear transactionMap
-    transactionMap.clear();
+    return new Connection(new pqxx::connection(*(this->connectString)));
+}
 
-    // destroy all connections
-    std::vector< boost::shared_ptr<Connection> >::iterator itCons;
+void ConnectionManager::releaseConnection(const PConnection conn) {
+    delete conn;
+}
 
-    for (itCons = connections.begin(); itCons != connections.end(); ++itCons) {
-        try {
-            itCons->reset();
-        } catch (...) {
-            // that's not ok but we can't do anything
-            std::cerr << __PRETTY_FUNCTION__ << "caught ..." << std::endl;
-        }
+ConnectionManager::ConnectionManager() {
+    this->isReady = false;
+};
+
+ConnectionManager::ConnectionManager(const ConnectionManager &org) {
+    throw new std::domain_error("Copy constructor not supported.");
+}
+
+void ConnectionManager::setupManager(const std::string &user, const std::string &password,
+                                     const std::string &database, const std::string &host) {
+    this->setupManager(user, password, database, host, -1);
+}
+
+void ConnectionManager::setupManager(const std::string &user, const std::string &password,
+                                     const std::string &database, const std::string &host,
+                                     const int32_t port) {
+    std::stringstream ss;
+
+    if (user.size() > 0) {
+        ss << " user=" << user << " ";
     }
 
-    // clear connection vector
-    connections.clear();
-    unusedConnections.clear();
-}
-
-//! \brief creates amount new connections in connection pool
-//!
-//! \param amount
-//!     the amount of connections to be added
-void ConnectionManager::createConnections(int amount) {
-    boost::recursive_mutex::scoped_lock lock(mutex); // protect
-
-    for (int atCon = 0; atCon != amount; ++atCon) {
-        bool created = false;
-
-        // try to store the new connection on a position where a
-        // previous connection was deleted
-        for (unsigned int atExCon = 0; atExCon != connections.size(); ++atExCon) {
-            if (!connections.at(atExCon)) {
-                // found a position -> store here
-                connections.at(atExCon) = boost::shared_ptr<Connection>(new Connection(con_info));
-                unusedConnections.push_back(atExCon);
-                created = true;
-            }
-        }
-
-        if (!created) {
-            // did not find any position -> store at end
-            connections.push_back(boost::shared_ptr<Connection>(new Connection(con_info)));
-            unusedConnections.push_back(connections.size() - 1);
-        }
+    if (password.size() > 0) {
+        ss << " password=" << password << " ";
     }
-}
 
-//! \brief transaction has finished and connection can be reused
-//!
-//! indicates that the transaction from the transactionholder
-//! has been aborted/committed and that the connection can
-//! be set to the state 'unused'
-//!
-//! this function must not throw, it will be called from
-//! the TransactionHolder destructor!
-//!
-void ConnectionManager::doneTransaction(int index) throw() {
-    // protect marking connection as unused
-    boost::recursive_mutex::scoped_lock lock(mutex);
-
-    // mark as unused
-    unusedConnections.push_back(index);
-    transactionMap.erase(index);
-}
-
-
-//! \brief create transaction on next free Connection
-//!
-//! does check if the next free connection is good,
-//! if not, this connection is dropped and another one used
-//!
-//! ~TransactionHolder releases the Connection
-//!
-//! \returns
-//!     a TransactionHolder which can be used to retrieve a
-//!     transaction. if this TransactionHolder is destructed
-//!     the connection is released for next usage
-//!
-TransactionHolder ConnectionManager::getTransaction() {
-    boost::recursive_mutex::scoped_lock lock(mutex); // protect retrieval of new connection
-
-    int index;
-    boost::shared_ptr<Connection> used_con;
-
-    // this loop is needed to remove connections
-    // which got lost/destroyed from the pool
-    do {
-        // create a connection if there is no one left unused
-        if (unusedConnections.size() == 0) {
-            //std::cerr << "creating new connection" << std::endl;
-            createConnections(1);
-        }
-
-        // get index of next unused connection
-        index = unusedConnections.back();
-
-        // use connection
-        unusedConnections.pop_back();
-        used_con = connections.at(index);
-
-        // remove connection completely if it is not good
-        if (!used_con->isGood()) {
-            std::cerr << "had to drop bad database connection!" << std::endl <<
-                      " (no harm done - creating another one)" << std::endl;
-            connections.at(index).reset(); // sets to 0
-            used_con.reset();
-        }
-    } while (!used_con);
-
-    // create transaction
-    Transaction *ptr = new Transaction(*used_con);
-    boost::shared_ptr<Transaction> new_trns(ptr);
-
-    boost::weak_ptr<Transaction> weak_trns = new_trns;
-    transactionMap[index] = new_trns;
-
-    // this transactionholder really is very tricky!
-    return TransactionHolder(new_trns, this_weak, index);
-}
-
-//! \brief constructor
-//!
-//! \param _trns
-//!     the transaction to hold (public)
-//! \param _con_mgr
-//!     the ConnectionManager where to release the connection
-//! \param _index
-//!     the index of this connection in the ConnectionManager
-//!
-ConnectionManager::TransactionHolder::TransactionHolder_::TransactionHolder_(
-    boost::shared_ptr<Transaction> _trns,
-    boost::weak_ptr<ConnectionManager> _con_mgr,
-    int _index):
-    trns(_trns), con_mgr(_con_mgr), index(_index) {}
-
-//! \brief destructor
-//!
-//! releases the connection in the ConnectionManager
-ConnectionManager::TransactionHolder::TransactionHolder_::~TransactionHolder_() throw() {
-    try {
-        // may throw boost::bad_weak_ptr
-        boost::shared_ptr<ConnectionManager> pcon_mgr(con_mgr);
-
-        // this calls ~trns -> weak pointer in transactionMap is invalid,
-        // but that does not matter
-        trns.reset();
-
-        // unregister *this
-        pcon_mgr->doneTransaction(this->index);
-    } catch (const boost::bad_weak_ptr &) {
-        // that's ok, don't unregister
-        std::cerr << __PRETTY_FUNCTION__ << "connection manager already destroyed" << std::endl;
-    } catch (...) {
-        // that's not ok, but we can't do anything
-        std::cerr << __PRETTY_FUNCTION__ << "caught ..." << std::endl;
+    if (database.size() > 0) {
+        ss << " dbname=" << database << " ";
     }
-}
 
-ConnectionManager::TransactionHolder::TransactionHolder(
-    boost::shared_ptr<Transaction> _trns,
-    boost::weak_ptr<ConnectionManager> _con_mgr,
-    int _index):
-    data(new TransactionHolder_(_trns, _con_mgr, _index)) {}
+    if (host.size() > 0) {
+        ss << " host=" << host << " ";
+    }
 
-// destructor
-ConnectionManager::TransactionHolder::~TransactionHolder() {}
+    if (port >= 0) {
+        ss << " port=" << port << " ";
+    }
 
-// casts itself to the lowlevel transaction
-ConnectionManager::TransactionHolder::operator Transaction&() {
-    return *data;
-}
-
-// casts itself to the lowlevel transaction
-Transaction &ConnectionManager::TransactionHolder::get() {
-    return *data;
-}
-
-int ConnectionManager::TransactionHolder::getIndex() const {
-    return data->getIndex();
+    this->connectString = new std::string(ss.str());
+    this->isReady = true;
 }
--- db/ConnectionManager.hpp
+++ db/ConnectionManager.hpp
@@ -16,270 +16,62 @@
 //  You should have received a copy of the GNU General Public License
 //  along with illarionserver.  If not, see <http://www.gnu.org/licenses/>.
 
+#ifndef _CONNECTION_MANAGER_HPP_
+#define _CONNECTION_MANAGER_HPP_
 
-#include <vector>
+#include <string>
 
-// database interface
-#include "di.hpp"
-#include "postgres.hpp"
+#include <dbng/Connection.hpp>
 
-#include <boost/shared_ptr.hpp>
-#include <boost/weak_ptr.hpp>
-#include <boost/thread.hpp>
+#include <boost/cstdint.hpp>
 
-namespace currentdb = ::di::postgres;
+namespace Database {
+class ConnectionManager;
 
-//! \brief used to maintain a high number of connections and to reuse them
-//!
-//! \todo cleanup sweep
-//!     (regularly check connections for 'isalive')
-//! \todo reduce sweep
-//!     (remove unused connections if there are too many of them)
+typedef ConnectionManager *PConnectionManager;
+
 class ConnectionManager {
-public:
-    //! the connection used for the database
-    typedef currentdb::Connection Connection;
-    //! a transaction used for the database
-    typedef currentdb::Transaction Transaction;
-
-public:
-    //! \brief constructor replacement (factory function)
-    //!
-    //! \param user
-    //!     the database user, may be ""
-    //! \param password
-    //!     the password of the user, may be ""
-    //! \param database
-    //!     the database for the connection, must not be ""!
-    //! \param host
-    //!     the hostname for the connection, may be "" for localhost
-    //! \param port
-    //!     the port for the connection, may be ""
-    //!
-    //! this function creates the initial connections
-    //!
-    static
-    boost::shared_ptr<ConnectionManager> CreateConnectionManager(
-        const std::string &user = "",
-        const std::string &password = "",
-        const std::string &database = "illarionserver",
-        const std::string &host = "",
-        const std::string &port = "",
-        int initialConnectionCount = 3);
-
-    //! \brief destructor - kills all used connections, frees all unused
-    //!
-    ~ConnectionManager() throw();
-
 private:
-    //! \brief constructor (private, construct with CreateConnectionManager)
-    //!
-    //! \param user
-    //!     the database user, may be ""
-    //! \param password
-    //!     the password of the user, may be ""
-    //! \param database
-    //!     the database for the connection, must not be ""!
-    //! \param host
-    //!     the hostname for the connection, may be "" for localhost
-    //! \param port
-    //!     the port for the connection, may be ""
-    //!
-    ConnectionManager(
-        const std::string &user = "",
-        const std::string &password = "",
-        const std::string &database = "illarionserver",
-        const std::string &host = "",
-        const std::string &port = "",
-        int initialConnectionCount = 1);
+    /* Singleton instance */
+    static ConnectionManager instance;
 
-    //! ConnectionManager must not be copy constructed !
-    ConnectionManager(const ConnectionManager &) {}
+    /* Connection String that is used to establish a connection to the
+     * postgre DB.
+     */
+    std::string *connectString;
+
+    /* Ready flag. This is set true once the connection informations are
+     * set.
+     */
+    bool isReady;
 
 public:
-    //! \brief the holder of a pointer to a transactionholder
-    //!
-    //! this class really is a shared pointer pointing to a
-    //! sub-transaction-holder and capable of casting itself
-    //! to the lowlevel transaction held by the sub-holder
-    //!
-    class TransactionHolder {
-    protected:
-        //! \brief the holder of a ``used'' connection with a transaction
-        //!
-        //! this class releases the connection after leaving the scope
-        //! this is the sub-holder-class of TransactionHolder
-        //!
-        class TransactionHolder_ {
-        public:
-            //! \brief constructor
-            //!
-            //! \param _trns
-            //!     the transaction to hold (public)
-            //! \param _con_mgr
-            //!     the ConnectionManager where to release the connection
-            //! \param _index
-            //!     the index of this connection in the ConnectionManager
-            //!
-            TransactionHolder_(
-                boost::shared_ptr<Transaction> _trns,
-                boost::weak_ptr<ConnectionManager> _con_mgr,
-                int _index);
+    /* Get the singleton instance of this class. */
+    static PConnectionManager getInstance(void);
 
-            //! \brief destructor
-            //!
-            //! releases the connection in the ConnectionManager
-            //!
-            virtual ~TransactionHolder_() throw();
+    /* Get a new connection. This function throws a std::domain_error in
+     * case the informations for the connection are not set yet.
+     */
+    PConnection getConnection(void);
 
-            //! \brief retrieves the Transaction
-            //!
-            //! \returns
-            //!     the held Transaction
-            //!
-            boost::weak_ptr<Transaction> getTransaction() {
-                return trns;
-            }
+    /* Release a connection. After this call the connection instance must
+     * not be used anymore. Else a crash is certain.
+     */
+    void releaseConnection(const PConnection conn);
 
-            //! \brief casts itself to Transaction
-            //!
-            operator Transaction&() {
-                return *trns.get();
-            }
+    /* Setup the connection informations to the postgre database. Once this
+     * data is set its possible to establish connections to the database.
+     */
+    void setupManager(const std::string &user, const std::string &password,
+                      const std::string &database, const std::string &host);
+    void setupManager(const std::string &user, const std::string &password,
+                      const std::string &database, const std::string &host,
+                      const int32_t port);
+private:
+    ConnectionManager(void);
+    ConnectionManager(const ConnectionManager &org);
 
-            //! \brief returns the index it was initialized with
-            int getIndex() const {
-                return index;
-            }
-
-        protected:
-            //! the held transaction
-            boost::shared_ptr<Transaction> trns;
-
-            //! the manager of the connection
-            //! this must be weak to allow destruction of the manager
-            boost::weak_ptr<ConnectionManager> con_mgr;
-
-            //! the index of the used connection in the manager
-            int index;
-
-        private:
-            //! TransactionHolder_ must not be copy constructed !
-            TransactionHolder_(const TransactionHolder_ &) {}}
-        ;
-
-    protected:
-        boost::shared_ptr<TransactionHolder_> data;
-
-    public:
-        //! \brief constructor
-        //!
-        //! \param _trns
-        //!     the transaction to hold (public)
-        //! \param _con_mgr
-        //!     the ConnectionManager where to release the connection
-        //! \param _index
-        //!     the index of this connection in the ConnectionManager
-        //!
-        TransactionHolder(
-            boost::shared_ptr<Transaction> _trns,
-            boost::weak_ptr<ConnectionManager> _con_mgr,
-            int _index);
-
-        //! \brief destructor
-        //!
-        virtual ~TransactionHolder();
-
-        //! \brief casts itself to Transaction&
-        //!
-        operator Transaction&();
-
-        //! \brief casts itself to Transaction&
-        //!
-        Transaction &get();
-
-        //! \brief returns the index stored
-        int getIndex() const;
-
-        //! \brief forward the quoting to the db_interface transaction
-        template< typename T >
-        std::string quote(const T &var) {
-            return data->operator Transaction&().quote(var);
-        }
-
-        //! \brief forward the quoting to the db_interface transaction
-        std::string formatNextval(const std::string &seqName) {
-            return data->operator Transaction&().formatNextval(seqName);
-        }
-
-        //! \brief forward the commit to the real transaction
-        void commit() {
-            data->operator Transaction&().commit();
-        }
-
-        //! \brief forward the rollback to the real transaction
-        void rollback() {
-            data->operator Transaction&().rollback();
-        }
-
-    };
-
-    //! \brief create transaction on next free Connection
-    //!
-    //! ~TransactionHolder releases the Connection
-    //!
-    //! \returns
-    //!     a TransactionHolder which can be used to retrieve a
-    //!     transaction. if this TransactionHolder is destructed
-    //!     the connection is released for next usage
-    //!
-    TransactionHolder getTransaction();
-
-protected:
-    //! the parameters given for connection creation
-    di::ConnectionInfo con_info;
-
-    //! the weak_ptr pointing to this manager
-    boost::weak_ptr<ConnectionManager> this_weak;
-
-protected:
-    //! mutex for managing the connections
-    boost::recursive_mutex mutex;
-
-    //! the currently opened connections
-    std::vector< boost::shared_ptr<Connection> > connections;
-    //! the assigned transactions
-    //! this must be weak to allow destruction of the transactions
-    std::map< int, boost::weak_ptr<Transaction> > transactionMap;
-
-    //! indexes of unused connections
-    std::vector< int > unusedConnections;
-
-protected:
-    //! \brief creates amount new connections in connection pool
-    //!
-    //! \param amount
-    //!     the amount of connections to be added
-    void createConnections(int amount);
-
-    //! \brief transaction has finished and connection can be reused
-    //!
-    //! indicates that the transaction from the transactionholder
-    //! has been aborted/committed and that the connection can
-    //! be set to the state 'unused'
-    //!
-    //! this function must not throw, it will be called from
-    //! the TransactionHolder destructor!
-    //!
-    void doneTransaction(int index) throw();
 };
+}
 
-typedef ConnectionManager::Connection ManagerConnection;
-typedef ConnectionManager::Transaction Transaction;
-typedef ConnectionManager::TransactionHolder TransactionHolder;
-
-//! the connection manager used in the database interface
-//! (it is threading-safe)
-extern boost::shared_ptr<ConnectionManager> dbmgr;
-//! and the connection manager for the account db
-extern boost::shared_ptr<ConnectionManager> accdbmgr;
+#endif // _CONNECTION_MANAGER_HPP_
