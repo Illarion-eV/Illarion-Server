378e279f44aa63db33afa1237af6385cde8ca421
Martin Karing <nitram@illarion.org>
2011-07-07 01:02:10 +0200

    Properly implemented new database system to common table

--- data/CommonObjectTable.cpp
+++ data/CommonObjectTable.cpp
@@ -17,17 +17,20 @@
 //  along with illarionserver.  If not, see <http://www.gnu.org/licenses/>.
 
 
+#include "CommonObjectTable.hpp"
+
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+
 #include "db/ConnectionManager.hpp"
 #include "db/SchemaHelper.hpp"
-#include "db/Query.hpp"
+#include "db/SelectQuery.hpp"
 #include "db/Result.hpp"
 
-#include "CommonObjectTable.hpp"
-#include <iostream>
-#include "World.hpp"
 #include "script/LuaItemScript.hpp"
-#include <boost/shared_ptr.hpp>
+
 #include "Logger.hpp"
+#include "World.hpp"
 
 CommonObjectTable::CommonObjectTable() : m_dataOK(false) {
     reload();
@@ -69,19 +72,21 @@ void CommonObjectTable::reload() {
 
     try {
         Database::PConnection connection =
-            Database::ConnectionManager::getInstance()->getConnection();
+            Database::ConnectionManager::getInstance().getConnection();
 
-        std::stringstream ss;
-        ss << "SELECT ";
-        ss << "\"com_itemid\", \"com_volume\", \"com_weight\", ";
-        ss << "\"com_agingspeed\", \"com_objectafterrot\", \"com_script\", ";
-        ss << "\"com_stackable\", \"com_rotsininventory\", ";
-        ss << "\"com_brightness\", \"com_worth\" ";
-        ss << "FROM ";
-        ss << Database::SchemaHelper::getServerSchema() << ".\"common\";";
-        
-        Database::PQuery query = new Database::Query(connection, ss.str());
-        Database::Result results = query->execute();
+        Database::SelectQuery query(*connection);
+        query.addColumn("common", "com_itemid");
+        query.addColumn("common", "com_volume");
+        query.addColumn("common", "com_weight");
+        query.addColumn("common", "com_agingspeed");
+        query.addColumn("common", "com_objectafterrot");
+        query.addColumn("common", "com_script");
+        query.addColumn("common", "com_brightness");
+        query.addColumn("common", "com_worth");
+        query.addServerTable("common");
+
+        Database::Result results = query.execute();
+        Database::ConnectionManager::getInstance().releaseConnection(connection);
 
         std::map<TYPE_OF_ITEM_ID, bool> assigned; // map item id to whether an infinite rot has been assigned
         std::map<TYPE_OF_ITEM_ID, bool> visited;  // map item id to whether it has been visited already in calcInfiniteRot
@@ -89,24 +94,25 @@ void CommonObjectTable::reload() {
         if (!results.empty()) {
             clearOldTable();
             CommonStruct temprecord;
-            
+
             for (Database::Result::ConstIterator itr = results.begin();
-                itr != results.end(); ++itr) {
+                 itr != results.end(); ++itr) {
 
                 TYPE_OF_ITEM_ID itemID;
-                itemID = (TYPE_OF_ITEM_ID) ((*itr)["com_itemid"].as<int16_t>());
+                itemID = (TYPE_OF_ITEM_ID)((*itr)["com_itemid"].as<int16_t>());
                 temprecord.id = itemID;
-                temprecord.Weight = (TYPE_OF_WEIGHT) ((*itr)["com_weight"].as<int16_t>());
-                temprecord.Volume = (TYPE_OF_VOLUME) ((*itr)["com_volume"].as<int16_t>());
-                temprecord.AgeingSpeed = (TYPE_OF_AGEINGSPEED) ((*itr)["com_agingspeed"].as<int16_t>());
-                temprecord.ObjectAfterRot = (TYPE_OF_ITEM_ID) (*itr)["com_objectafterrot"].as<TYPE_OF_ITEM_ID>();
+                temprecord.Weight = (TYPE_OF_WEIGHT)((*itr)["com_weight"].as<int16_t>());
+                temprecord.Volume = (TYPE_OF_VOLUME)((*itr)["com_volume"].as<int16_t>());
+                temprecord.AgeingSpeed = (TYPE_OF_AGEINGSPEED)((*itr)["com_agingspeed"].as<int16_t>());
+                temprecord.ObjectAfterRot = (TYPE_OF_ITEM_ID)(*itr)["com_objectafterrot"].as<TYPE_OF_ITEM_ID>();
                 temprecord.isStackable = (*itr)["com_stackable"].as<bool>();
                 temprecord.rotsInInventory = (*itr)["com_rotsininventory"].as<bool>();
-                temprecord.Brightness = (TYPE_OF_BRIGHTNESS) ((*itr)["com_brightness"].as<int16_t>());
-                temprecord.Worth = (TYPE_OF_WORTH) ((*itr)["com_worth"].as<int16_t>());
+                temprecord.Brightness = (TYPE_OF_BRIGHTNESS)((*itr)["com_brightness"].as<int16_t>());
+                temprecord.Worth = (TYPE_OF_WORTH)((*itr)["com_worth"].as<int16_t>());
 
                 if (!((*itr)["com_worth"].is_null())) {
                     std::string scriptname = ((*itr)["com_worth"].as<std::string>());
+
                     try {
                         boost::shared_ptr<LuaItemScript> tmpScript(new LuaItemScript(scriptname, temprecord));
                         m_scripttable[itemID] = tmpScript;
@@ -114,12 +120,13 @@ void CommonObjectTable::reload() {
                         Logger::writeError("scripts", "Error while loading script: " + scriptname + ":\n" + e.what() + "\n");
                     }
                 }
-                    
+
                 m_table[itemID] = temprecord;
 
                 visited.insert(std::pair<TYPE_OF_ITEM_ID, bool>(itemID,false));
                 assigned.insert(std::pair<TYPE_OF_ITEM_ID, bool>(itemID,false));
             }
+
             m_dataOK = true;
         } else {
             m_dataOK = false;
