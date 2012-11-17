d3a78844e49c51ced3a8ee6020793464f4e59e9c
vilarion <vilarion@illarion.org>
2012-11-17 19:58:27 +0100

    Fix group IDs in CraftingDialog

--- dialog/CraftingDialog.hpp
+++ dialog/CraftingDialog.hpp
@@ -63,8 +63,8 @@ private:
     uint8_t craftedStackSize;
 
 public:
-    Craftable(uint8_t group, TYPE_OF_ITEM_ID item, string name, uint16_t decisecondsToCraft): item(item), name(name), decisecondsToCraft(decisecondsToCraft), craftedStackSize(1) {};
-    Craftable(uint8_t group, TYPE_OF_ITEM_ID item, string name, uint16_t decisecondsToCraft, uint8_t craftedStackSize): item(item), name(name), decisecondsToCraft(decisecondsToCraft), craftedStackSize(craftedStackSize) {};
+    Craftable(uint8_t group, TYPE_OF_ITEM_ID item, string name, uint16_t decisecondsToCraft): group(group), item(item), name(name), decisecondsToCraft(decisecondsToCraft), craftedStackSize(1) {};
+    Craftable(uint8_t group, TYPE_OF_ITEM_ID item, string name, uint16_t decisecondsToCraft, uint8_t craftedStackSize): group(group), item(item), name(name), decisecondsToCraft(decisecondsToCraft), craftedStackSize(craftedStackSize) {};
     Craftable(const Craftable &craftable) {
         group = craftable.group;
         item = craftable.item;
