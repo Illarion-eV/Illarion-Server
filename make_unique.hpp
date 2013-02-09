214ebd67c02a789619929400ff24722688a67373
vilarion <vilarion@illarion.org>
2013-02-09 17:45:54 +0100

    astyle

--- make_unique.hpp
+++ make_unique.hpp
@@ -24,11 +24,12 @@
 #include <utility>
 
 namespace std {
-    template<typename T, typename ...Args>
-    std::unique_ptr<T> make_unique( Args&& ...args )
-    {
-        return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
-    }
+
+template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique(Args&& ...args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
 }
 
 #endif
