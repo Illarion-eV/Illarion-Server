f576495255d3808e7b3b4dac53d8be225887c53e
vilarion <vilarion@illarion.org>
2013-03-01 19:24:39 +0100

    Update demo scripts

--- setup/scripts/server/itemlookat.lua
+++ setup/scripts/server/itemlookat.lua
@@ -1,18 +1,17 @@
 -- This script is the central item lookat.
 
-require("base.lookat")
-
 module("server.itemlookat", package.seeall)
 
 --- Item Lookat
--- The task of this function is to generate the lookat on a item.
+-- The task of this function is to generate a default lookat for an item.
 -- This function is called by the server.
 --
 -- @param[in] player 	the player that is supposed to receive the inform
 -- @param[in] item      the item the player is looking at
--- @return TRUE in case the script generated the lookat
+-- @return true in case the script generated the lookat
 function lookAtItem(player, item)
-	local lookAt = base.lookat.GenerateLookAt(player, item)
-	world:itemInform(player, item, lookAt)
-	return true
+    local lookAt = lookAt()
+    lookAt.name = world:getItemName(item.id,Player.english)
+    world:itemInform(player, item, lookAt)
+    return true
 end
--- setup/scripts/server/reload.lua
+++ setup/scripts/server/reload.lua
@@ -1,13 +1,8 @@
--- called after every !rd command and !fr command
--- note that !rd is a !fr without npc and spawn reloading
--- note further that reload_tables will be called after this if and only if the !rd was successful and the !rd was issued by a !fr
-
-
+-- called after every !fr command
 
 module("server.reload", package.seeall)
 
 function onReload()
-    return true;
+    return true
 end
-
 
