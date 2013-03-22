5fbd66db454bafa93a0b23774f5d40e6ee08b7d3
vilarion <vilarion@illarion.org>
2013-03-22 19:43:28 +0100

    Remove redundant include

--- Random.cpp
+++ Random.cpp
@@ -16,7 +16,6 @@
 //  You should have received a copy of the GNU General Public License
 //  along with illarionserver.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <functional>
 #include "Random.hpp"
 
 std::mt19937 Random::rng;
