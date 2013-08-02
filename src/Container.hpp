e4dae04176842c6b6cf8b103ddb9ff6d85cf6f29
vilarion <vilarion@illarion.org>
2013-08-02 04:46:55 +0200

    Fix mantis issue #9008 - auto-pickup and bags

--- src/Container.hpp
+++ src/Container.hpp
@@ -87,7 +87,7 @@ public:
 
     int weight();
 
-    virtual TYPE_OF_CONTAINERSLOTS getSlotCount();
+    virtual TYPE_OF_CONTAINERSLOTS getSlotCount() const;
 
     inline const ITEMMAP &getItems() const {
         return items;
@@ -95,6 +95,8 @@ public:
     inline const CONTAINERMAP &getContainers() const {
         return containers;
     }
+
+    TYPE_OF_CONTAINERSLOTS getFirstFreeSlot() const;
 
 private:
     bool isItemStackable(Item item);
