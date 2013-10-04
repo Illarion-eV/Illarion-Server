72339ce625c378cd8755ca70db7fbcee750f76fa
vilarion <vilarion@illarion.org>
2013-10-04 18:01:53 +0200

    Change license to GNU AGPL v3

--- src/db/Connection.cpp
+++ src/db/Connection.cpp
@@ -11,7 +11,7 @@
  *
  * Illarionserver is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY;  without  even  the  implied  warranty  of  MERCHANTABILITY  or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU  General Public License along with
