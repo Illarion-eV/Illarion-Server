59bbbbecf4bd6a8ef6d890b3b072d5f39b96fc09
vilarion <vilarion@illarion.org>
2013-10-13 17:24:17 +0200

    Change license to GNU AGPL v3

--- src/data/QuestTable.hpp
+++ src/data/QuestTable.hpp
@@ -5,7 +5,7 @@
  * This file is part of Illarionserver.
  *
  * Illarionserver  is  free  software:  you can redistribute it and/or modify it
- * under the terms of the  GNU  General  Public License as published by the Free
+ * under the terms of the  GNU Affero General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
  *
@@ -14,7 +14,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  *
- * You should have received a copy of the GNU  General Public License along with
+ * You should have received a copy of the GNU Affero General Public License along with
  * Illarionserver. If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _QUEST_TABLE_HPP_
