693410fe1de438cd4764e8f82dab73237d0fca51
vilarion <vilarion@illarion.org>
2021-07-31 04:38:18 +0200

    Set C++ standard to C++20

--- tools/run-cppcheck.bat
+++ tools/run-cppcheck.bat
@@ -1,4 +1,4 @@
 @echo off
 pushd %~dp0
-docker-compose -f cppcheck.yml run --rm cppcheck sh -c "cppcheck --std=c++17 --enable=warning,style,performance,portability --inconclusive -j $(nproc) --error-exitcode=1 -q --suppress=functionStatic:src/db/InsertQuery.hpp -Isrc/ src/"
+docker-compose -f cppcheck.yml run --rm cppcheck sh -c "cppcheck --std=c++20 --enable=warning,style,performance,portability --inconclusive -j $(nproc) --error-exitcode=1 -q --suppress=functionStatic:src/db/InsertQuery.hpp -Isrc/ src/"
 popd
