181a4d54971fadf39e1875be901f1c58601d87b7
vilarion <vilarion@illarion.org>
2021-08-10 08:45:33 +0200

    Set new player spawn location

--- setup/illarion.sql
+++ setup/illarion.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 11.7 (Debian 11.7-0+deb10u1)
--- Dumped by pg_dump version 11.7 (Debian 11.7-0+deb10u1)
+-- Dumped from database version 11.12 (Debian 11.12-0+deb10u1)
+-- Dumped by pg_dump version 11.12 (Debian 11.12-0+deb10u1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -15,6 +15,33 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- Name: illarion; Type: DATABASE; Schema: -; Owner: -
+--
+
+CREATE DATABASE illarion WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C' LC_CTYPE = 'C';
+
+
+\connect illarion
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE illarion; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON DATABASE illarion IS 'contains all illarion data';
+
 
 --
 -- Name: accounts; Type: SCHEMA; Schema: -; Owner: -
@@ -1390,10 +1417,10 @@ CREATE SEQUENCE server.owned_maps_seq
 
 CREATE TABLE server.player (
     ply_playerid integer NOT NULL,
-    ply_posx smallint DEFAULT 31 NOT NULL,
-    ply_posy smallint DEFAULT 22 NOT NULL,
-    ply_posz smallint DEFAULT 100 NOT NULL,
-    ply_faceto smallint DEFAULT 0 NOT NULL,
+    ply_posx smallint DEFAULT 702 NOT NULL,
+    ply_posy smallint DEFAULT 283 NOT NULL,
+    ply_posz smallint DEFAULT 0 NOT NULL,
+    ply_faceto smallint DEFAULT 4 NOT NULL,
     ply_age smallint DEFAULT 0 NOT NULL,
     ply_weight integer DEFAULT 0 NOT NULL,
     ply_body_height smallint DEFAULT (0)::smallint NOT NULL,
