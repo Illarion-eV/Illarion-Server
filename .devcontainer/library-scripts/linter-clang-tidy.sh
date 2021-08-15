88da9661653bcce709b8c577c063d8023c222690
vilarion <vilarion@illarion.org>
2021-08-15 16:13:57 +0000

    Pin tool versions

--- .devcontainer/library-scripts/linter-clang-tidy.sh
+++ .devcontainer/library-scripts/linter-clang-tidy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 LINTER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-"${LINTER_DIR}/linter-common.sh" "ghcr.io/illarion-ev/illarion-server-tools-clang:latest" "clang-tidy" "clang" "$@"
+"${LINTER_DIR}/linter-common.sh" "ghcr.io/illarion-ev/illarion-server-tools-clang:sha-b185f1e1" "clang-tidy" "clang" "$@"
--- .devcontainer/library-scripts/linter-cppcheck.sh
+++ .devcontainer/library-scripts/linter-cppcheck.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 LINTER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-"${LINTER_DIR}/linter-common.sh" "ghcr.io/illarion-ev/illarion-server-tools-cppcheck:latest" "cppcheck" "cppcheck" "$@"
+"${LINTER_DIR}/linter-common.sh" "ghcr.io/illarion-ev/illarion-server-tools-cppcheck:sha-94c41a15" "cppcheck" "cppcheck" "$@"
